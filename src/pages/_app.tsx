a33aaae98d23234f2897a9fe002cdef40c962136
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-20 12:15:55 -0300

    importando components header e player para arquivo principal do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/global.scss'
+import { Header } from "../components/Header/Index";
+import { Player } from "../components/Player/Index";
+import styles from '../styles/app.module.scss';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+
+  );
 }
 
 export default MyApp;
