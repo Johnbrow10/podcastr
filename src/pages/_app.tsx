db5478eb26841fe81d3a5c25735dcdda5d7a4fbc
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-23 20:37:44 -0300

    Criacao da function play, seta o episodio atual e depois zera ele para produzir um novo, e tem o UseState para sobrescrever essas variaveis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import { Header } from "../components/Header/Index";
 import { Player } from "../components/Player/Index";
+import { PlayerContext } from "../contexts/PlayerContext";
 import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
 
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
+  return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
