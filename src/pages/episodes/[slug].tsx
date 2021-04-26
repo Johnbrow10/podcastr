7a24efc22f64d9b86101198451bd91d09fb003a2
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-26 16:19:24 -0300

    Botao para reproduzir podcast solicitado na pagina

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,12 +1,12 @@
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router'
+import { usePlayer } from '../../contexts/PlayerContext';
 import { api } from '../../services/api';
-import Image from 'next/image'
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
-import styles from './episode.module.scss'
+import styles from './episode.module.scss';
 
 type Episode = {
   id: string;
@@ -27,6 +27,8 @@ type EpisodeProps = {
 
 export default function Episode({ episode }: EpisodeProps) {
 
+  const { play } = usePlayer();
+
   return (
     < div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -36,7 +38,7 @@ export default function Episode({ episode }: EpisodeProps) {
           </button>
         </Link>
         <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
-        <button type="button" >
+        <button type="button" onClick={() => play(episode)} >
           <img src="/play.svg" alt="Tocar Episodio" />
         </button>
       </div>
