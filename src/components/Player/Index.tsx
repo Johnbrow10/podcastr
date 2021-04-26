4f5dbb28c3a7da89674b6b623133b0db2fdfc515
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-26 10:01:25 -0300

    Adicionando tag de audio para reproduzir audios

--- src/components/Player/Index.tsx
+++ src/components/Player/Index.tsx
@@ -57,20 +57,27 @@ export function Player() {
           <span>00:00</span>
         </div>
 
+        {episode && (
+          <audio
+            src={episode.url}
+            autoPlay
+          />
+        )}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar Anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button type="button" className={styles.playButton} disabled={!episode}>
             <img src="/play.svg" alt="Tocar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar Proxima" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button type="button" className={styles.playButton} disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
